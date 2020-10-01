1b559a77f3a94d610647aa60d8d16a1dbe221d1b
Simon Pickles <simon.pickles@kaluza.com>
2020-10-01 19:04:31 +0100

    Revert port, debug print

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const app = express();
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+const port = process.env.PORT || 8080;
+console.log("Using port", port);
+
 // Start the app by listening on the default Heroku port
-app.listen(80);
+app.listen(port);
