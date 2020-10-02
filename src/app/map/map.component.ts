875493e28f136f368b49d8d8ad2d0feb8193ab2d
Simon Pickles <simon.pickles@kaluza.com>
2020-10-02 02:20:55 +0100

    poly

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps'
 
 @Component({
   selector: 'app-map',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
+
   zoom = 3.8
+  
   center: google.maps.LatLngLiteral = { 
     lat: 40, 
     lng: -40,
   };
+
   options: google.maps.MapOptions = {
-    //mapTypeId: 'hybrid',
     zoomControl: false,
     scrollwheel: false,
     disableDoubleClickZoom: true,
@@ -21,9 +24,26 @@ export class MapComponent implements OnInit {
     disableDefaultUI: true,
   }
 
+  delims = [
+    { lng: -30,  lat: 30 },
+    { lng: -30, lat: 50 },
+    { lng: -50, lat: 50 },
+    { lng: -50, lat: 30 },
+  ];
+
+  poly = new google.maps.Polygon({
+    paths: this.delims,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 3,
+    fillColor: '#FF0000',
+    fillOpacity: 1.0
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+    this.poly.setMap(this.map.googleMap);
   }
 
 }
