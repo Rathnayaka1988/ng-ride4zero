2c949c39f272d79b87dc8a0ab2a4f9f6ed788a07
Simon Pickles <simon.pickles@kaluza.com>
2020-10-02 01:26:55 +0100

    Pull totals from backend, 2 calls, so bad

--- src/app/co2-panel/co2-panel.component.ts
+++ src/app/co2-panel/co2-panel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
+import { Total } from '../models';
 
 @Component({
   selector: 'app-co2-panel',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./co2-panel.component.scss']
 })
 export class Co2PanelComponent implements OnInit {
+  value: number = 791;
+  totals$: Observable<Total>;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.totals$ = this.data.getTotals();
+    this.totals$.subscribe(totals => {
+      console.log("Co2PanelComponent got totals", totals);
+      this.value = totals.carbon_g;
+    });
   }
 
 }
--- src/app/distance-panel/distance-panel.component.ts
+++ src/app/distance-panel/distance-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
+import { Total } from '../models';
+
 
 @Component({
   selector: 'app-distance-panel',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./distance-panel.component.scss']
 })
 export class DistancePanelComponent implements OnInit {
+  value: number = 1342;
+  totals$: Observable<Total>;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.totals$ = this.data.getTotals();
+    this.totals$.subscribe(totals => {
+      console.log("DistancePanelComponent got totals", totals);
+      this.value = totals.distance_m;
+    });
   }
-
 }
